f322a3c04553bda019d85d4be1104243b6094a12
ulugbekdev1 <kuziboyevulugbek02@gmail.com>
2026-04-18 10:14:24 +0500

    update

--- carrer_back/config/urls.py
+++ carrer_back/config/urls.py
@@ -1,28 +1,17 @@
-from django.contrib import admin
-from django.urls import path, include, re_path
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from django.conf import settings
-from django.conf.urls.static import static
-from rest_framework_simplejwt.views import TokenRefreshView
-from django.views.generic import TemplateView
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('quiz.urls')),
-    path('api/', include('users.urls')),
-    path('api/', include('certificate.urls')),
+
+    # 🟢 API APPS (TO‘G‘RI STRUCTURE)
+    path('api/quiz/', include('quiz.urls')),
+    path('api/users/', include('users.urls')),
+    path('api/certificate/', include('certificate.urls')),
+
+    # 🔐 AUTH
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # 📄 DOCS
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema')),
-]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-# React frontend catch-all (must be last)
-urlpatterns += [
-    re_path(r'^(?!api/|admin/|file/|static/).*$',
-            TemplateView.as_view(template_name='index.html'),
-            name='react'),
+    # ⚛️ FRONTEND
 ]
